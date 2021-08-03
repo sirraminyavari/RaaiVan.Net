a1487d2052602750892bf00d7b0fb2e1c6d8b171
Ramin <sir.raminyavari@gmail.com>
2021-08-03 14:31:49 +0430

    modernized Events module data layer

---
+++ Modules/GlobalUtilities/DBCall/Events/EVTParsers.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RaaiVan.Modules.GlobalUtilities;
+using RaaiVan.Modules.Users;
+
+namespace RaaiVan.Modules.Events
+{
+    public static class EVTParsers
+    {
+        public static List<Event> events(DBResultSet results, bool full = false)
+        {
+            List<Event> retList = new List<Event>();
+
+            RVDataTable table = results.get_table();
+
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                Event item = new Event() {
+                    EventID = table.GetGuid(i, "EventID"),
+                    Title = table.GetString(i, "Title")
+                };
+
+                if (full)
+                {
+                    item.EventType = table.GetString(i, "EventType");
+                    item.Description = table.GetString(i, "Description");
+                    item.BeginDate = table.GetDate(i, "BeginDate");
+                    item.FinishDate = table.GetDate(i, "FinishDate");
+                    item.CreatorUserID = table.GetGuid(i, "CreatorUserID");
+                }
+
+                retList.Add(item);
+            }
+
+            return retList;
+        }
+
+        public static List<RelatedUser> related_users(DBResultSet results, bool userInfo = false, bool eventInfo = false)
+        {
+            List<RelatedUser> retList = new List<RelatedUser>();
+
+            RVDataTable table = results.get_table();
+
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                RelatedUser relatedUser = new RelatedUser() {
+                    UserInfo = new User() {
+                        UserID = table.GetGuid(i, "UserID")
+                    },
+                    EventInfo = new Event() {
+                        EventID = table.GetGuid(i, "EventID")
+                    },
+                    Status = table.GetString(i, "Status"),
+                    Done = table.GetBool(i, "Done"),
+                    RealFinishDate = table.GetDate(i, "RealFinishDate")
+                };
+
+                if (userInfo)
+                {
+                    relatedUser.UserInfo.UserName = table.GetString(i, "UserName");
+                    relatedUser.UserInfo.FirstName = table.GetString(i, "FirstName");
+                    relatedUser.UserInfo.LastName = table.GetString(i, "LastName");
+                }
+
+                if (eventInfo)
+                {
+                    relatedUser.EventInfo.EventType = table.GetString(i, "EventType");
+                    relatedUser.EventInfo.Title = table.GetString(i, "Title");
+                    relatedUser.EventInfo.Description = table.GetString(i, "Description");
+                    relatedUser.EventInfo.BeginDate = table.GetDate(i, "BeginDate");
+                    relatedUser.EventInfo.FinishDate = table.GetDate(i, "FinishDate");
+                    relatedUser.EventInfo.CreatorUserID = table.GetGuid(i, "CreatorUserID");
+                }
+
+                retList.Add(relatedUser);
+            }
+
+            return retList;
+        }
+    }
+}
---
+++ Modules/GlobalUtilities/DBCall/Events/EventsController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RaaiVan.Modules.Knowledge;
+using RaaiVan.Modules.CoreNetwork;
+using RaaiVan.Modules.GlobalUtilities;
+
+namespace RaaiVan.Modules.Events
+{
+    public static class EventsController
+    {
+        private static string GetFullyQualifiedName(string name) => "[dbo]." + "[EVT_" + name + "]"; //'[dbo].' is database owner and 'EVT_' is module qualifier
+
+        public static bool create_event(Guid applicationId, Event info, List<Guid> userIds, List<Guid> groupIds, List<Guid> nodeIds)
+        {
+            List<Guid> relatedNodeIds = new List<Guid>();
+
+            relatedNodeIds.AddRange(groupIds);
+            relatedNodeIds.AddRange(nodeIds);
+
+            return DBConnector.succeed(applicationId, GetFullyQualifiedName("CreateEvent"),
+                applicationId, info.EventID, info.EventType, info.OwnerID, info.Title, info.Description, 
+                info.BeginDate, info.FinishDate, info.CreatorUserID, info.CreationDate, 
+                ProviderUtil.list_to_string<Guid>(relatedNodeIds), ProviderUtil.list_to_string<Guid>(userIds), ',');
+        }
+
+        public static bool remove_event(Guid applicationId, Guid eventId)
+        {
+            return DBConnector.succeed(applicationId, GetFullyQualifiedName("ArithmeticDeleteEvent"), applicationId, eventId);
+        }
+
+        public static List<Event> get_events(Guid applicationId, List<Guid> eventIds, bool? full = false)
+        {
+            DBResultSet results = DBConnector.read(applicationId, GetFullyQualifiedName("GetEventsByIDs"),
+                applicationId, ProviderUtil.list_to_string<Guid>(eventIds), ',', full);
+
+            return EVTParsers.events(results, full: full.HasValue && full.Value);
+        }
+
+        public static Event get_event(Guid applicationId, Guid eventId, bool? full = false)
+        {
+            return get_events(applicationId, new List<Guid>() { eventId }, full).FirstOrDefault();
+        }
+
+        public static int get_user_finished_events_count(Guid applicationId, Guid userId, bool? done = null)
+        {
+            return DBConnector.get_int(applicationId, GetFullyQualifiedName("GetUserFinishedEventsCount"),
+                applicationId, userId, DateTime.Now, done);
+        }
+
+        public static List<Event> get_user_finished_events(Guid applicationId, Guid userId, bool? done = null)
+        {
+            return EVTParsers.events(DBConnector.read(applicationId, GetFullyQualifiedName("GetUserFinishedEvents"),
+                applicationId, userId, DateTime.Now, done));
+        }
+
+        public static List<Guid> get_related_user_ids(Guid applicationId, Guid eventId)
+        {
+            return DBConnector.get_guid_list(applicationId, GetFullyQualifiedName("GetRelatedUserIDs"), applicationId, eventId);
+        }
+
+        public static List<RelatedUser> get_related_users(Guid applicationId, Guid eventId)
+        {
+            DBResultSet results = DBConnector.read(applicationId, GetFullyQualifiedName("GetRelatedUsers"), applicationId, eventId);
+            return EVTParsers.related_users(results, userInfo: true);
+        }
+
+        public static bool remove_related_user(Guid applicationId, Guid eventId, Guid userId, ref bool calenderDeleted)
+        {
+            int result = DBConnector.get_int(applicationId, GetFullyQualifiedName("ArithmeticDeleteRelatedUser"),
+                applicationId, eventId, userId);
+
+            calenderDeleted = result == 2 ? true : false;
+
+            return result > 0 ? true : false;
+        }
+
+        public static bool change_user_status(Guid applicationId, Guid eventId, Guid userId, string status)
+        {
+            return DBConnector.succeed(applicationId, GetFullyQualifiedName("ChangeUserStatus"),
+                applicationId, eventId, userId, status);
+        }
+
+        public static List<Node> get_related_nodes(Guid applicationId, Guid eventId, NodeTypes? nodeType = null)
+        {
+            string strNodeTypeId = null;
+            if (nodeType.HasValue) strNodeTypeId = CNUtilities.get_node_type_additional_id(nodeType.Value).ToString();
+
+            List<Guid> nodeIds = DBConnector.get_guid_list(applicationId, GetFullyQualifiedName("GetRelatedNodeIDs"),
+                applicationId, eventId, strNodeTypeId);
+
+            return CNController.get_nodes(applicationId, nodeIds, full: null, currentUserId: null);
+        }
+
+        public static List<Event> get_node_related_events(Guid applicationId, 
+            Guid nodeId, DateTime? beginDate = null, bool? notFinished = null)
+        {
+            return EVTParsers.events(DBConnector.read(applicationId, GetFullyQualifiedName("GetNodeRelatedEvents"),
+                applicationId, nodeId, beginDate, notFinished));
+        }
+
+        public static List<RelatedUser> get_user_related_events(Guid applicationId, Guid userId, 
+            DateTime? beginDate = null, bool? notFinished = null, UserStatus? status = null, Guid? nodeId = null)
+        {
+            DBResultSet results = DBConnector.read(applicationId, GetFullyQualifiedName("GetUserRelatedEvents"),
+                applicationId, userId, beginDate, notFinished, status, nodeId);
+
+            return EVTParsers.related_users(results, userInfo: false, eventInfo: true);
+        }
+    }
+}
